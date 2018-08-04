d551bae520302c2cac46ce4d01c93e3d4f56e263
Dgrouse96 <dgrouse96@gmail.com>
2018-08-04 11:25:17 +1000

    Colormod fix
    added _add _mul

--- gamemode/client/views.lua
+++ gamemode/client/views.lua
@@ -16,80 +16,80 @@ local CamSmoothPos = Vector()
 local CamSmoothAng = Angle()
 
 function DoCameraAnim( Cams, MoveTime )
-	
+
 	if #Cams <= 0 then return end
-	
+
 	InCameraAnim = true
 	CameraAnimPos = Cams[1][1]
 	CameraAnimAng = Cams[1][2]
-	
+
 	CamSmoothPos = CameraAnimPos
 	CamSmoothAng = CameraAnimAng
-	
+
 	CameraAnimMoveTime = MoveTime
 	CameraAnimTime = 0
 	CameraAnimStart = CurTime()
 	CameraBlendAmount = 1 * ( #Cams - 1 )
-	
+
 	CameraCams = Cams
-	
+
 	hook.Add( "Think", "RunPoseAnims", ThinkPoseAnims )
-	
+
 	hook.Add( "CalcView", "CameraAnims", function( ply, pos, angles, fov )
-		
+
 		CameraAnimTime = CurTime() - CameraAnimStart
 		local LerpTime = inverselerpclamp( CameraAnimTime, 0, CameraAnimMoveTime )
-		
+
 		LerpTime = ease( LerpTime, 2, 0.5 )
-		
+
 		local CameraTime = LerpTime * CameraBlendAmount
-		
+
 		local Alpha = ease( CameraTime % 1, 1.5, Either( CameraTime < 1, 0, 0.5 ) )
-		
+
 		if LerpTime < 1 then
-		
+
 			local Current = CameraCams[ math.floor( CameraTime ) + 1 ]
 			local Next = CameraCams[ math.floor( CameraTime ) + 2 ]
-			
+
 			CameraAnimPos = LerpVector( Alpha, Current[1], Next[1] )
 			CameraAnimAng = LerpAngle( Alpha, Current[2], Next[2] )
-			
+
 		else
-			
+
 			CameraAnimPos = CameraCams[ #CameraCams ][1]
 			CameraAnimAng = CameraCams[ #CameraCams ][2]
-			
+
 		end
-		
+
 		CamSmoothPos = LerpVector( FrameTime() * 5, CamSmoothPos, CameraAnimPos )
 		CamSmoothAng = LerpAngle( FrameTime() * 5, CamSmoothAng, CameraAnimAng )
-		
+
 		return {
-			
+
 			origin = CamSmoothPos,
 			angles = CamSmoothAng,
 			fov = 70,
 			drawviewer = true,
 		}
-		
+
 	end )
-	
+
 end
 
 local function StopCameraAnim()
-	
+
 	hook.Remove( "CalcView", "CameraAnims" )
 	InCameraAnim = false
 	UpdateThirdPerson()
-	
+
 end
 hook.Add( "StopCameraAnim", "FromServer", StopCameraAnim )
 
 
 local function ThirdPersonView( ply, pos, angles, fov )
-	
+
 	if InCameraAnim then return end -- Will need to make a state machine eventually
-	
+
 	local view = {}
 
 	//view.origin = pos + Vector( 0, 0, 40 ) + angles:Forward() * 150
@@ -129,39 +129,45 @@ end
 
 
 function UpdateThirdPerson()
-	
+
 	if LocalPlayer().IsThirdPerson and LocalPlayer():IsThirdPerson() then
-		
+
 		hook.Add( "CalcView", "ThirdPerson", ThirdPersonView )
 		hook.Add( "Think", "RunPoseAnims", ThinkPoseAnims )
-	
+
 	else
-		
+
 		hook.Remove( "CalcView", "ThirdPerson" )
 		hook.Remove( "Think", "RunPoseAnims" )
-		
+
 	end
-	
+
 end
 
 -- hacky asf
 timer.Simple( 2, UpdateThirdPerson )
 
 concommand.Add( "gg_thirdperson", function( args )
-	
+
 	LocalSettings:Input( "thirdperson", !LocalSettings:GetData()[ "thirdperson" ], true )
 	UpdateThirdPerson()
-	
+
 end )
 
 
 -- Temporary fix
 if game.GetMap() == "gr_crossroads" then
-	
+
 	RunConsoleCommand( "pp_colormod", "1" )
 	RunConsoleCommand( "pp_colormod_contrast", "1.4" )
 	RunConsoleCommand( "pp_colormod_brightness", "0.1" )
-	
+	RunConsoleCommand( "pp_colormod_addr", "0" )
+	RunConsoleCommand( "pp_colormod_addg", "0" )
+	RunConsoleCommand( "pp_colormod_addb", "0" )
+	RunConsoleCommand( "pp_colormod_mulr", "1" )
+	RunConsoleCommand( "pp_colormod_mulg", "1" )
+	RunConsoleCommand( "pp_colormod_mulb", "1" )
+
 	RunConsoleCommand( "pp_bloom", "1" )
 	RunConsoleCommand( "pp_bloom_multiply", "1" )
 	RunConsoleCommand( "pp_bloom_darken", "0.5" )
@@ -170,5 +176,5 @@ else
 
 	RunConsoleCommand( "pp_colormod", "0" )
 	RunConsoleCommand( "pp_bloom", "0" )
-	
+
 end
--- gamemode/data/localsettings.lua
+++ gamemode/data/localsettings.lua
@@ -5,39 +5,45 @@
 LocalSettings = GData( "localsettings" )
 
 local function Setting()
-	
+
 	return LocalSettings:GetData()
-	
+
 end
+
+
+--
+-- Player functions
+--
 
 local PLY = FindMetaTable( "Player" )
 
+
 function PLY:IsThirdPerson()
-	
+
 	if self != LocalPlayer() then return false end
-	
+
 	if Setting()[ "thirdperson" ] or InCameraAnim then
-		
+
 		return true
-		
+
 	end
-	
+
 	return false
-	
+
 end
 
 
 -- Shhhh pay no attention here
 function UpdateAimbot()
-	
+
 	if Setting()[ "aimbot" ] then
-		
+
 		sendRequest( "ImAHacker" )
-		
+
 	else
 
 		LocalSettings:Input( "aimbot", false, true )
-		
+
 	end
 
 end
